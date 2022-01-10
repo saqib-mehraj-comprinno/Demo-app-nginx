9cdd05dc92e29c8481e46201d50e0295c0715f6d
SaqibMehraj <84028792+SaqibMehraj@users.noreply.github.com>
2022-01-11 04:26:44 +0530

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-aws eks update-kubeconfig --region us-east-1 --name dev-eks-cluster
-#kubectl create ns test
-kubectl get pods --all-namespaces
-#kubectl create deployment nginx --image nginx -n test
-#kubectl get namespaces
-kubectl create -f k8nginx.yaml -n jenkins
